08d7eaedf57eaffac87552fa62afef81954b21d8
舒克卷不动啦 <976347841@qq.com>
2025-12-16 15:17:46 +0800

    -整合了rag，实现整体async

--- main.py
+++ main.py
@@ -1,36 +1,30 @@
 import asyncio
-
 from v2_rag_with_stream_async import *
-from rag_with_SQLALchemy_table import *
-import rag_with_SQLALchemy_table
-from Sql_base import *
-url = 'mysql+aiomysql://shuke:123456@localhost/sk_db?charset=utf8mb4'
-engine = create_async_engine(url, echo=True)
-print(engine)
-#初始化asyncsessionlocal
-rag_with_SQLALchemy_table.AsyncSessionLocal=async_sessionmaker(bind = engine,class_=AsyncSession)
-#测试保存信息
-async def test_save():
-    await db_save_message(session_id="test001", role="human",
-                          content="你是什么模型")
-    await db_save_message(session_id="test001", role="ai",
-                          content="你好，有什么可以帮助你的？")
-    await db_save_message(session_id="test001", role="human",
-                          content="今天天气怎么样")
-    print('对话保存成功')
-async def test_load():
-    history = await db_load_history(session_id="test001")
-    print('加载到的数据量: ',len(history.messages))
-    for msg in history.messages:
-        print(f'{type(msg).__name__}:{msg.content}')
-def main_test():
-    local_embed = load_vector_store("1201Faiss.faiss")
-    retrieval = local_embed.as_retriever(search_kwargs={'k': 3})
-async def test_load1():
-    await test_load()
-    await engine.dispose()
-async def test_save1():
-    await test_save()
-    await engine.dispose()
+from rag_kb_management import KnowledgeService,KnowledgeBaseManager,DocumentProcessor
+from v3_rerank_rag_private import SimpleRerank
+from typing import Optional
+from rag_config import rerank_url,get_device,llm
+from rag_with_async_table import invoke_save,engine,get_rag_chain_session
+device = get_device()
+async def main(kb_path,
+               kb_name,
+               session_id:str,
+               _rerank= True):
+    ks = KnowledgeService(
+        KnowledgeBaseManager(kb_path_or_name=kb_path), document_processor=DocumentProcessor())
+    vector_store=ks.search_kb(kb_name)
+    retriever = vector_store.as_retriever(search_kwargs={'k': 3})
+    if _rerank:
+        rerank = SimpleRerank(model_name_or_path =rerank_url,
+                              base_retriever=retriever
+                              )
+        history_aware_retriever=create_history_aware_retriever_chain(llm = llm, retriever=rerank)
+        qa_chain = create_qa_chain( llm = llm,history_aware_retriever = history_aware_retriever)
+        qa_chain = get_rag_chain_session(rag_chain=qa_chain,get_session=get_session_history)
+        await invoke_save('test1',rag=qa_chain)
+        await engine.dispose()
+
+
 if __name__ == '__main__':
-    asyncio.run(test_save1())
+    kb_path_or_name = '/Users/salutethedawn/Desktop/编程用文件夹/西瓜/hw项目跟敲/private项目/rag-chatbot/kb/kb_list'
+    asyncio.run(main(kb_path=kb_path_or_name,kb_name = '技术文档',session_id = 'test12'))
